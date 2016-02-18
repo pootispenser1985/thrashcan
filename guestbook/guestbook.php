6582bcbbba8ce03a693c28644632a2a13338fb3e
root <root@CouchWizard>
2016-02-17 21:29:58 -0600

    styled guestbook

--- guestbook/guestbook.php
+++ guestbook/guestbook.php
@@ -1,19 +1,37 @@
 <!DOCTYPE html>
 <html>
-<head><title>Guestbook!</title></head>
+<head>
+  <title>Guestbook!</title>
+  <link rel="stylesheet" type="text/css" href="../main.css">
+</head>
 <body>
-  SIGN THE FUCKING GUESTBOOK. DO IT NOW.
+  <div id="container">
 
-  Prior Blerbz:
+    <div id="menu">
+      <ul>
+        <li><a href="flashcards/flashcards.php">Flashcard Thingy</a></li>
+        <li><a href="guestbook/guestbook.php">Guestbook</a></li>
+        <li><a href="#">Placeholder Two</a></li>
+      </ul>
+    </div>
+    <h1>SIGN THE FUCKING GUESTBOOK. DO IT NOW.</h1>
 
-  <?php
-    $db = new SQLite3("comments.db");
-    $query = "SELECT * FROM comments";
-    $result = $db->query($query);
-    $row = $result->fetchArray();
+    <p>Prior Blerbz:</p>
 
-    echo $row[1]." ".$row[2];
+
+
+
+
+
+    <?php
+      $db = new SQLite3("comments.db");
+      $query = "SELECT * FROM comments";
+      $result = $db->query($query);
+      $row = $result->fetchArray();
+
+      echo $row[1]." ".$row[2];
 
    ?>
+  </div>
 </body>
 </html>
