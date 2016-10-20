e8b4bc2c1f42f93fe7349f28e6ccb25b76e0cc5d
pootispenser1985 <jigostowski@gmail.com>
2016-10-19 23:15:16 -0500

    .

---
+++ guestbook/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<!--  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12./jquery.min.js"></script>  -->
+  <script src="http://www.thrashcan.net/includes/jquery-3.1.1.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css"
+    href="https://fonts.googleapis.com/css?family=Open Sans">
+  <link rel="stylesheet" type="text/css" href="main.css?version=80">
+  <link rel="shortcut icon" href="http://thrashcan.net/pics/dogturd-transparent.png">
+  <title>THRASHCAN</title>
+</head>
+<body>
+  <div class="container">
+    <nav class="row">
+      <div class="col-xs-3 col-md-2"><a href="/guestbook/index.php">Guestbook</a></div>
+      <div class="col-md-2 hidden-xs hidden-sm"><a href="#">Placeholder</a></div>
+      <div class="col-xs-3 col-md-2"><a href="/pics/pics.html">Pictures</a></div>
+      <div class="col-xs-6 col-md-2 col-md-offset-4"><a href="http://www.thrashcan.net">Thrash</a>
+        <img src="/pics/dogturd.png"></div>
+    </nav>
+
+    <div class="row">
+    </div>
+  </div>
+</body>
+</html>
